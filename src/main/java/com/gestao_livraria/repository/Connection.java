8fe47c82e3acdb95b7a1020bafbcfb61110b5b97
mrtn <martinlanga@icloud.com>
2026-03-22 04:35:50 +0200

    feat: connection string

---
+++ src/main/java/com/gestao_livraria/repository/Connection.java
@@ -0,0 +1,10 @@
+package com.gestao_livraria.repository;
+
+public class Connection {
+
+    private static final String URL  = "jdbc:postgresql://localhost:5432/biblioteca_db";
+    private final String username = "";
+    private final String password = "";
+
+
+}
